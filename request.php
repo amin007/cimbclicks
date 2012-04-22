78c597754fc786e38239d88f0c10dc44755f5a04
Leow Kah Thong <ktleow@gmail.com>
2012-04-22 11:43:24 +0800

    Simplified the wrapper class.

--- request.php
+++ request.php
@@ -5,18 +5,18 @@ $cimbClicks = new CimbClicks('PAYEE_ID');
 <!doctype html>
 <html>
 <head>
-  <title>CIMB Clicks example - Request</title>
+    <title>CIMB Clicks example - Request</title>
 </head>
 <body>
-  <form action="<?php echo $cimbClicks->getPaymentUrl(); ?>" method="get">
-    <input type="text" name="payeeId" value="<?php echo $cimbClicks->getPayeeId(); ?>" />
-    <input type="text" name="billAccountNo" value="ABC0000000001" />
-    <input type="text" name="billReferenceNo" value="XYZ0000000001" />
-    <input type="text" name="billReferenceNo2" value="" />
-    <input type="text" name="billReferenceNo3" value="" />
-    <input type="text" name="billReferenceNo4" value="" />
-    <input type="text" name="amount" value="1.00" />
-    <input type="submit" value="Pay with CIMB Clicks" />
-  </form>
+    <form action="<?php echo $cimbClicks->getPaymentUrl(); ?>" method="get">
+        <input type="text" name="payeeId" value="<?php echo $cimbClicks->getPayeeId(); ?>" />
+        <input type="text" name="billAccountNo" value="ABC0000000001" />
+        <input type="text" name="billReferenceNo" value="XYZ0000000001" />
+        <input type="text" name="billReferenceNo2" value="" />
+        <input type="text" name="billReferenceNo3" value="" />
+        <input type="text" name="billReferenceNo4" value="" />
+        <input type="text" name="amount" value="1.00" />
+        <input type="submit" value="Pay with CIMB Clicks" />
+    </form>
 </body>
 </html>
--- response.php
+++ response.php
@@ -3,12 +3,3 @@ require_once './CimbClicks.class.php';
 $cimbClicks = new CimbClicks('PAYEE_ID');
 $response = $cimbClicks->getPaymentResponse();
 var_dump($response);  // Use this to debug
-?>
-<!doctype html>
-<html>
-<head>
-  <title>CIMB Clicks example - Response</title>
-</head>
-<body>
-</body>
-</html>
